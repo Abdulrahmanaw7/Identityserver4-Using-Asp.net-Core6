0b89b670531c6ecb3ce82b5daabc5374de9283d0
DEVOPSolution <DEVOPSolution@DESKTOP-F5A4KKB>
2022-06-22 17:25:23 +0200

    Add project files.

---
+++ IdentityServer4.Auth/IdentityConfiguration.cs
@@ -0,0 +1,78 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace IdentityServer4.Auth
+{
+    public class IdentityConfiguration
+    {
+        public static IEnumerable<IdentityResource> IdentityResources()
+        {
+            return new IdentityResource[]
+            {
+            new IdentityResources.OpenId(),
+        new IdentityResources.Profile()
+
+
+    };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new[]
+              {
+        new TestUser
+        {
+            SubjectId = "1",
+            Username = "mail@filipekberg.se",
+            Password = "password"
+
+        }
+            };
+        }
+
+        internal static IEnumerable<Client> Clients()
+        {
+            return new[]
+            {
+
+        new Client
+        {
+            ClientId = "IdentityServer4",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            AllowedScopes = new [] { "IdentityServer4" }
+        },
+         new Client
+        {
+            ClientId = "IdentityServer4_MVC",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+            AllowedGrantTypes = GrantTypes.Code,
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:44329/signin-oidc" },
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:44329/signout-callback-oidc" },
+              AllowedScopes = new [] {
+                  IdentityServerConstants.StandardScopes.OpenId,
+                  IdentityServerConstants.StandardScopes.Profile,
+                  "IdentityServer4" }
+        }
+            };
+        }
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+    new List<ApiScope>
+    {
+        new ApiScope("IdentityServer4", "Identity Server4")
+    };
+
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("IdentityServer4", "Identity Server4")
+            };
+        }
+
+    }
+}
---
+++ MyWeb/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWeb.Controllers
+{
+    [Authorize]
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //private async Task RefreshTokenAsync()
+        //{
+        //    var authrizationServerInformation =
+        //        await DiscoveryClient.GetAsync("https://localhost:44358");
+        //    var client = new TokenClient(authrizationServerInformation.TokenEndpoint, "IdentityServer4", "secret");
+        //    var refreshToken= await HttpContext.authantication
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
